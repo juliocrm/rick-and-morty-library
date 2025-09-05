ae491e3d3101077aadb2428b7f3870b42562aa47
Julio Rodriguez <julcrm@gmail.com>
2025-09-04 23:00:19 -0400

    Sets up Apollo Client and UI framework
    Configures Apollo Client to connect to the Rick and Morty GraphQL API.
    Wraps the application with ApolloProvider and BrowserRouter to enable data fetching and routing.

---
+++ src/apollo/client.js
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { HttpLink } from '@apollo/client/link/http';
+
+const httpLink = new HttpLink({
+  uri: 'https://rickandmortyapi.com/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+export default client;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import { ApolloProvider } from '@apollo/client/react';
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react'
+import './styles/index.css'
 import App from './App.jsx'
+import client from './apollo/client.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
 )
